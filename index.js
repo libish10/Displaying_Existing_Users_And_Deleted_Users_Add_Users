fee3a127905beb92137ad426e008de1e8f3d2493
libish10123 <libishjm@gmail.com>
2021-09-25 12:21:49 +0530

    Add_Delete

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-function Validate(event) {
-    alert("hai");
 
-
-
-}
 DisplayUser()
 
 function DisplayUser() {
@@ -23,11 +18,65 @@ function DisplayUsersList(data) {
         var Result = "";
         data.map((user) => {
             let fname = user.first_name ? user.first_name : user.fist_name;
-
-            let lname = (user.last_name)
-            Result += "<div class='user_details'><p class='first_letter'>" + lname + "</p></div>";
-            //Result += "<div class='user_details'><p class='first_letter'>" + fname.substr(0, 1) + "</p><p>" + " " + fname + " " + lname + "</p></div>";
+            let email = user.email;
+            let lname = user.last_name;
+           
+            Result += `<div class='grid-item'><p  class="grid-items-name">${fname} ${lname}</p><i class="fas fa-pencil-alt"></i><i class="fas fa-trash-alt" onclick='RemoveUser("${user.email}")'></i></div>`
         })
         document.getElementById("displayingExistingUser").innerHTML = Result;
     }
+}
+
+function RemoveUser(emailId) {
+    console.log(emailId);
+    let dataObj = { email: emailId };
+    let url = "http://3.6.93.159:7883/machstatz/delete_existing_user";
+    fetch(url, {
+            method: "DELETE",
+            body: JSON.stringify(dataObj),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log("Error : ", err));
+
+
+
+
+}
+
+
+function AddUser() {
+          let   fname=`"${document.getElementById("fName").value}"`;
+          let   lname=`"${document.getElementById("lName").value}"`;
+          let   Uname=`"${document.getElementById("uName").value}"`;
+          let   emailId=`"${document.getElementById("eMail").value}"`;
+          let   password=`"${document.getElementById("passWord").value}"`;
+          console.log(fname);
+          console.log(lname);
+          console.log(Uname);
+          console.log(emailId);
+          console.log(passWord);
+          let headers=new Headers();
+              headers.append('Access-Control-Allow-Origin', "http://3.6.93.159:7883/machstatz/add_new_user");
+               headers.append('Access-Control-Allow-Credentials', 'true');
+          let url="http://3.6.93.159:7883/machstatz/add_new_user";
+          fetch(url,{
+              method:"POST",
+              body: JSON.stringify({
+                    email:  emailId,
+                    fist_name: fname,
+                    last_name: lname,
+                    pwd: passWord,
+                    username: Uname,
+                }),
+               
+          })
+           .then((response) => response.json())
+            .then((json) => console.log(json))
+            .catch((message)=>console.log("message : ",message));
+
+    return false;
 }
